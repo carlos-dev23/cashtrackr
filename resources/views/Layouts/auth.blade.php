27e7e6019ea759dace129913a50c9585e9f53b62
carlos-dev23 <jcsegurat@gmail.com>
2026-08-02 23:17:30 -0600

    feat(auth): add login and register forms with base layouts

---
+++ resources/views/Layouts/auth.blade.php
@@ -0,0 +1,6 @@
+@extends('Layouts.base')
+@section('contents')
+    <main class="max-w-2xl mt-10 mx-auto p-10 shadow-lg">
+        @yield('auth-contents')
+    </main>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/auth/register', function () {
+    return view('Auth.register');
+});
+
+Route::get('/auth/login', function () {
+    return view('Auth.login');
+});
