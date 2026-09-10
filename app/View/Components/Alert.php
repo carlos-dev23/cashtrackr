76cdb62ef4531dc7179f90e880235876b72a671f
carlos-dev23 <jcsegurat@gmail.com>
2026-09-09 22:20:12 -0600

    fix(components): set default values for Alert component parameters

--- app/View/Components/Alert.php
+++ app/View/Components/Alert.php
@@ -12,8 +12,8 @@ class Alert extends Component
      * Create a new component instance.
      */
     public function __construct(
-        public string $type,
-        public string $message
+        public string $type = '',
+        public string $message = ''
     ) {
         //
     }
