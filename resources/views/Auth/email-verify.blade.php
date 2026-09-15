e9fbcb4460b3f02a74fb3616956f150acebe75b7
carlos-dev23 <jcsegurat@gmail.com>
2026-09-14 20:27:04 -0600

    feat(auth): add email verification notification route and update email verification view

--- resources/views/Auth/email-verify.blade.php
+++ resources/views/Auth/email-verify.blade.php
@@ -4,4 +4,11 @@
 @endsection
 @section('auth-contents')
     <p class="mt-5 text-lg">Tu cuenta fue creada con exito. Ahora solo debes confirmarla, revisa tu e-mail</p>
+    @if (session('error'))
+        <x-alert :message="session('error')" />
+    @endif
+    <form method="POST" action="{{ route('verification.send') }}">
+        <input type="submit" class="bg-amber-500 w-full text-center mt-5 px-5 py-2 uppercase font-bold cursor-pointer"
+            value="Reenviar correo de verificación" />
+    </form>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -20,6 +21,11 @@ Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $requ
     return redirect()->route('dashboard')->with('success', 'Tu correo fue verificado correctamente.
     Ya puedes crear Presupuestos y Gastos');
 })->middleware(['auth', 'signed'])->name('verification.verify');
+
+Route::get('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+    return back()->with('success', 'Se ha enviado un nuevo correo de verificación a tu cuenta.');
+})->middleware(['auth','throttle:1 ,1'])->name('verification.send');
 
 Route::get('/email/verify', function () {
     return view('Auth.email-verify');
